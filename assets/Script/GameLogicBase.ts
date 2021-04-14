13adc6d04747b544dcce43f4a453a8c8aff8f449
GPN-Phat <phat@gpn.com.vn>
2021-04-14 12:47:20 +0700

    Updated

--- assets/Script/GameLogicBase.ts
+++ assets/Script/GameLogicBase.ts
@@ -188,7 +188,7 @@ export default class GameLogicBase extends cc.Component {
     OnGameOver(result: boolean) {
         this.score = (this.LIFE - this.gameUI.checkCount) * 100;
         var userData = cc.sys.localStorage.getItem('userData');
-        if (userData == "null" || userData == "undefined") {
+        if (userData == null || userData == undefined) {
             this.RegisterUserData();
             userData = JSON.parse(cc.sys.localStorage.getItem('userData'));
         }
